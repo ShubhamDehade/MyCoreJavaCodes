18a3b3cbc961f579a80f886f45abaa36c61951a0
Shubham Dehade <128866398+ShubhamDehade@users.noreply.github.com>
2023-03-27 09:48:50 +0530

    Add files via upload

---
+++ src/StringDemo/StringBufferBuilder.java
@@ -0,0 +1,16 @@
+package StringDemo;
+
+public class StringBufferBuilder {
+    public static void main(String[] args) {
+        String s1=new String("HELLO");//immutable
+        StringBuffer s2=new StringBuffer("HELLO");//mutable and thread safe i.e, synchronized
+        StringBuilder s3=new StringBuilder("HELLO");//mutable and thread unsafe
+
+        System.out.println( s1.concat("WORLD"));
+        System.out.println(s1);//hello
+        s2.append("world");
+        System.out.println(s2);
+        s3.append("world");
+        System.out.println(s3);
+    }
+}
