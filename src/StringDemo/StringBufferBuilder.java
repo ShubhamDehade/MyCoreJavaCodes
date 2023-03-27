43bf756cefd69a905b5adc2f4b4d016dca5c6446
Shubham Dehade <128866398+ShubhamDehade@users.noreply.github.com>
2023-03-27 09:52:12 +0530

    Update StringBufferBuilder.java

--- src/StringDemo/StringBufferBuilder.java
+++ src/StringDemo/StringBufferBuilder.java
@@ -6,11 +6,11 @@ public class StringBufferBuilder {
         StringBuffer s2=new StringBuffer("HELLO");//mutable and thread safe i.e, synchronized
         StringBuilder s3=new StringBuilder("HELLO");//mutable and thread unsafe
 
-        System.out.println( s1.concat("WORLD"));
+        System.out.println( s1.concat("WORLD"));//HELLOWORLD
         System.out.println(s1);//hello
         s2.append("world");
-        System.out.println(s2);
+        System.out.println(s2);//HELLOworld
         s3.append("world");
-        System.out.println(s3);
+        System.out.println(s3);//HELLOworld
     }
 }
